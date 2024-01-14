03633e59c018e19c5a3ea3898396356135c512db
Andrey Manuilov <dukariga@gmail.com>
2024-01-14 23:03:21 +0200

    Actions for check icon added
    Added action and updated extension to display green check icon in text fields on input

--- iOS12-HW11-Andrey-Manuilov/Sources/Extensions/UITextField+Extensions.swift
+++ iOS12-HW11-Andrey-Manuilov/Sources/Extensions/UITextField+Extensions.swift
@@ -11,4 +11,26 @@ extension UITextField {
         leftView = iconContainerView
         leftViewMode = .always
     }
+    
+    func setRightIcon(_ image: UIImage) {
+        let rightIconView = UIImageView()
+        rightIconView.contentMode = .scaleAspectFit
+        rightIconView.image = image
+        rightIconView.translatesAutoresizingMaskIntoConstraints = false
+
+        let iconContainerView = UIView()
+        iconContainerView.translatesAutoresizingMaskIntoConstraints = false
+        self.rightView = iconContainerView
+        self.rightViewMode = .always
+
+        iconContainerView.addSubview(rightIconView)
+        
+        iconContainerView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        iconContainerView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        rightIconView.centerYAnchor.constraint(equalTo: iconContainerView.centerYAnchor).isActive = true
+        rightIconView.trailingAnchor.constraint(equalTo: iconContainerView.trailingAnchor, constant: -15).isActive = true
+        rightIconView.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        rightIconView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
 }
