1334244d3a05299069b4b6ef83b69c16fcf99317
Andrey Manuilov <dukariga@gmail.com>
2024-01-14 15:26:26 +0200

    Initial commit with file structure, gitignore and readme

--- iOS12-HW11-Andrey-Manuilov/Sources/App/ViewController.swift
+++ iOS12-HW11-Andrey-Manuilov/Sources/App/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  iOS12-HW11-Andrey-Manuilov
-//
-//  Created by Andrey Manuilov on 14/01/2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,4 +9,3 @@ class ViewController: UIViewController {
 
 
 }
-
