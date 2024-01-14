1849208f97f93000047e1ab7c500ad0f01dc8c09
Andrey Manuilov <dukariga@gmail.com>
2024-01-14 14:51:13 +0200

    Initial Commit

---
+++ iOS12-HW11-Andrey-Manuilov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW11-Andrey-Manuilov
+//
+//  Created by Andrey Manuilov on 14/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
